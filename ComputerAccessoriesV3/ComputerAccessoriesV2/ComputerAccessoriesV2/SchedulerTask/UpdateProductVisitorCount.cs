4dbcafd47eb6ebb926743326a118e7d3c7f4bfa3
Bac Nguyen Xuan <16520068@gm.uit.edu.vn>
2020-01-03 21:49:49 +0700

    implement for 30p query update product visiter count

---
+++ ComputerAccessoriesV3/ComputerAccessoriesV2/ComputerAccessoriesV2/SchedulerTask/UpdateProductVisitorCount.cs
@@ -0,0 +1,48 @@
+﻿using ComputerAccessoriesV2.DI;
+using ComputerAccessoriesV2.Models;
+using ComputerAccessoriesV2.Ultilities;
+using Coravel.Invocable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ComputerAccessoriesV2.SchedulerTask
+{
+    public class UpdateProductVisitorCount : IInvocable
+    {
+        private readonly ComputerAccessoriesV2Context _db;
+        private readonly IRedis _redis;
+
+        public UpdateProductVisitorCount(ComputerAccessoriesV2Context db, IRedis redis)
+        {
+            _db = db;
+            _redis = redis;
+        }
+        public Task Invoke()
+        {
+            _db.Products.ToList().ForEach(p =>
+            {
+                try
+                {
+                    var cacheVisitorCount = int.Parse(_redis.GetValue(Constants.CACHE_PRODUCT_CURRENT_VIEWING_PREFIX + p.Id, "0"));
+                    if (cacheVisitorCount <= p.ViewCounts)
+                    {
+                        _redis.SetValue(Constants.CACHE_PRODUCT_CURRENT_VIEWING_PREFIX + p.Id, p.ViewCounts.ToString());
+                    }
+                    else
+                    {
+                        p.ViewCounts = cacheVisitorCount;
+                        _db.SaveChanges();
+                        _redis.Publish("notify", "Save done");
+                    }
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+            });
+            return Task.CompletedTask;
+        }
+    }
+}
